f7a5199ff8b95d70198f955ee5c30e2c644246dd
loanmatteusz <loan.matteusz@gmail.com>
2025-10-11 16:24:40 -0300

    feat: init and config initial setup to run dev script

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export { app };

b1e3124f8a757b7a57e854c50c76f8a3bdb9cd60
loanmatteusz <loan.matteusz@gmail.com>
2025-10-11 23:01:43 -0300

    feat: add and configure logger with winston, morgan, cors and cookieParser, then configure import format

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { logger } from '#config/logger';
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', {
+    stream: {
+        write: (message: string) => logger.info(message.trim()),
+    },
+}));
+
+app.get('/health-check', (_, response) => {
+    logger.info("health check is working");
+    response.status(200).send("Ok");
+});
 
 export { app };
